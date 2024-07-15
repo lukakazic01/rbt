c2c5adac5feb4ddac0a738510845f520fbb44038
lukakazic01 <kazicluka4@gmail.com>
2024-07-15 18:01:05 +0200

    made layout based on the task needs

--- src/app/task-2/task2.component.ts
+++ src/app/task-2/task2.component.ts
@@ -2,7 +2,6 @@ import {Component} from "@angular/core";
 
 @Component({
   templateUrl: './task2.component.html',
-  styleUrl: './task2.component.scss',
   selector: 'app-task2',
   standalone: true,
 })
