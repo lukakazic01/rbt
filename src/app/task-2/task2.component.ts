f8c5aa4e1a2ecf33b1c59d7b1d9b04ed40bf74fa
lukakazic01 <kazicluka4@gmail.com>
2024-07-15 19:40:08 +0200

    finished task 2

--- src/app/task-2/task2.component.ts
+++ src/app/task-2/task2.component.ts
@@ -1,11 +1,13 @@
-import {Component} from "@angular/core";
+import {Component, ElementRef, ViewChild} from "@angular/core";
 
 @Component({
   templateUrl: './task2.component.html',
   selector: 'app-task2',
   standalone: true,
 })
-
 export class Task2Component {
-
+  @ViewChild('table') tableEl: ElementRef<HTMLTableElement> | null = null;
+  scrollToTable() {
+    if(this.tableEl) this.tableEl.nativeElement?.scrollIntoView({behavior: 'smooth', block: 'start'});
+  }
 }
