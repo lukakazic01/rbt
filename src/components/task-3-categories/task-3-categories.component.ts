6a411bf5f95413b28ac855b06cbaad3c39a181dc
lukakazic01 <kazicluka4@gmail.com>
2024-07-16 08:53:39 +0200

    replaced takeUntil with takeUntilDestroyed

--- src/components/task-3-categories/task-3-categories.component.ts
+++ src/components/task-3-categories/task-3-categories.component.ts
@@ -1,8 +1,8 @@
-import {Component, OnDestroy, OnInit} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 import {Task3CategoriesService} from "./task-3-categories.service";
 import {Category} from "../../types/category";
-import {Subject, takeUntil} from "rxjs";
 import {CommonModule} from "@angular/common";
+import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
 
 
 @Component({
@@ -13,26 +13,20 @@ import {CommonModule} from "@angular/common";
   imports: [CommonModule]
 })
 
-export class Task3CategoriesComponent implements OnInit, OnDestroy{
+export class Task3CategoriesComponent implements OnInit{
 
   constructor(private task3CategoriesService: Task3CategoriesService) {
   }
 
   categories: Category[] | null = null
-  $destroy: Subject<boolean> = new Subject<boolean>();
 
   trackByCategory(index: number, category: Category) {
     return category.id
   }
 
   ngOnInit() {
-    this.task3CategoriesService.getAllCategories().pipe(takeUntil(this.$destroy)).subscribe({
+    this.task3CategoriesService.getAllCategories().pipe(takeUntilDestroyed()).subscribe({
       next: (res) => this.categories = res,
     })
-  }
-
-  ngOnDestroy() {
-    this.$destroy.next(true);
-    this.$destroy.unsubscribe();
   }
 }
--- src/views/task-3/task3.component.ts
+++ src/views/task-3/task3.component.ts
@@ -5,6 +5,7 @@ import {Movie} from "../../types/movie";
 import {Task3MovieCardComponent} from "../../components/task-3-movie-card/task-3-movie-card.component";
 import {Subject, takeUntil} from "rxjs";
 import {Task3CategoriesComponent} from "../../components/task-3-categories/task-3-categories.component";
+import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
 
 
 @Component({
@@ -15,26 +16,20 @@ import {Task3CategoriesComponent} from "../../components/task-3-categories/task-
   standalone: true,
 })
 
-export class Task3Component implements OnInit, OnDestroy{
+export class Task3Component implements OnInit{
 
   constructor(private task3Service: Task3Service) {
   }
 
-  private destroy$: Subject<boolean> = new Subject<boolean>();
   allMovies: Movie[] | null = null
 
   ngOnInit() {
-    this.task3Service.getAllMovies().pipe(takeUntil(this.destroy$)).subscribe({
+    this.task3Service.getAllMovies().pipe(takeUntilDestroyed()).subscribe({
       next: (res) => this.allMovies = res,
     })
   }
 
   trackByMovie(index: number, movie: Movie) {
     return parseInt(movie.id)
-  }
-
-  ngOnDestroy() {
-    this.destroy$.next(true);
-    this.destroy$.unsubscribe();
   }
 }
