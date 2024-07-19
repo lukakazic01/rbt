be4323668a48704995dcfc15186a7c932d78c3a4
lukakazic01 <kazicluka4@gmail.com>
2024-07-19 11:51:53 +0200

    Added routerLink in task , fragment didnt show up

--- src/views/task-2/task2.component.ts
+++ src/views/task-2/task2.component.ts
@@ -1,9 +1,13 @@
 import {Component, ElementRef, ViewChild} from "@angular/core";
+import {RouterLink} from "@angular/router";
 
 @Component({
   templateUrl: './task2.component.html',
   selector: 'app-task2',
   standalone: true,
+  imports: [
+    RouterLink
+  ]
 })
 export class Task2Component {
   @ViewChild('table') tableEl: ElementRef<HTMLTableElement> | null = null;
